c4ec6f373b01814b123c7b7a09ce8f8a70b3b411
Eatowl <csnelcov@gmail.com>
2016-11-26 02:04:37 +0700

    loop update

--- main.c
+++ main.c
@@ -19,13 +19,23 @@ char buffer[BUF_SIZE];
 struct thread_arg {
     WINDOW *my_wins[1];
     float count;
+    char cp_file[BUF_SIZE];
+};
+
+struct thread_arg_paste {
+    char cp_file_p[BUF_SIZE];
+    char paste_direct[BUF_SIZE];
 };
 
 char **words;
 char *directory,
      *save_directory,
      *temporary_directory,
-     *test;
+     *test,
+     *copy_file,
+     *copy_dir,
+     *name_copy_file;
+char *dir_array;
 
 struct dirent *entry;
 
@@ -35,8 +45,8 @@ struct dirent *entry;
 int type_file(char *directory, char **words, unsigned choice, WINDOW **my_wins);
 
 int main() {
-    WINDOW *my_wins[3];
-    PANEL  *my_panels[3];
+    WINDOW *my_wins[4];
+    PANEL  *my_panels[4];
     PANEL  *top;
 
     create_wins_and_panel(my_wins, my_panels);
@@ -46,6 +56,10 @@ int main() {
     save_directory = malloc_array(save_directory);
     temporary_directory = malloc_array(temporary_directory);
     test = malloc_array(test);
+    copy_file = (char*) malloc(4096);
+    copy_dir = (char*) malloc(4096);
+    dir_array = (char*) malloc(4096);
+    name_copy_file = (char*) malloc(128);
     words = (char**) malloc(SIZE_INCREMENT*sizeof(char*));
 
     unsigned choice = 0;
@@ -119,25 +133,6 @@ int main() {
                     update_dir = true;
                     choice = 0;
                 }
-                if (type_f == 1) {
-                    int tfd;
-                    ssize_t byt;
-                    float count = 0;
-                    tfd = open ("type.c", O_RDONLY);
-                    while ((byt = read (tfd, buffer, BUF_READ)) > 0)
-                        count++;
-                    close (tfd);
-
-                    pthread_t thread, thread1;
-                    int result, result1;
-                    struct thread_arg targ;
-
-                    targ.my_wins[0] = my_wins[2];
-                    targ.count = count;
-
-                    result1 = pthread_create(&thread1, NULL, &cp_func, NULL);
-                    result = pthread_create (&thread, NULL, &any_func, &targ);
-                }
                 if (type_f == 2) {
                     char *run;
                     unsigned run_length;
@@ -161,6 +156,61 @@ int main() {
                     execve (run, newprog_args, NULL);
                     free(run);
                 }
+                break;
+            case 99:  // Нажатие клавиши "C" - копирование файла
+                type_f = type_file(directory, words, choice, my_wins);
+                if (type_f == 1) {
+                    int tfd;
+                    ssize_t byt;
+                    float count = 0;
+                    tfd = open ("type.c", O_RDONLY);
+                    while ((byt = read (tfd, buffer, BUF_READ)) > 0)
+                        count++;
+                    close (tfd);
+
+                    strcpy(copy_dir, directory);
+                    strcpy(copy_file, directory);
+                    strcat(copy_file, "/");
+                    strcat(copy_file, words[choice]);
+                    strcpy(name_copy_file, words[choice]);
+
+                    pthread_t thread;
+                    int result;
+                    struct thread_arg targ;
+
+                    targ.my_wins[0] = my_wins[3];
+                    targ.count = count;
+                    strcpy(targ.cp_file, copy_file);
+
+                    result = pthread_create (&thread, NULL, &percent_func, &targ);
+                }
+                break;
+            case 112: // Нажатие клавиши "P" - вставить файл
+                strcpy(dir_array, directory);
+                if (strcmp(directory, copy_dir) == 0) {
+                    struct thread_arg_paste cp_arg;
+
+                    strcat(name_copy_file, "(copy)");
+                    strcat(dir_array, "/");
+                    strcat(dir_array, name_copy_file);
+                    strcpy(cp_arg.cp_file_p, copy_file);
+                    strcpy(cp_arg.paste_direct, dir_array);
+
+                    pthread_t thread1;
+                    int result1;
+                    result1 = pthread_create(&thread1, NULL, &cp_func, &cp_arg);
+                } else {
+                    struct thread_arg_paste cp_arg;
+
+                    strcat(dir_array, name_copy_file);
+                    strcpy(cp_arg.cp_file_p, copy_file);
+                    strcpy(cp_arg.paste_direct, dir_array);
+
+                    pthread_t thread1;
+                    int result1;
+                    result1 = pthread_create(&thread1, NULL, &cp_func, &cp_arg);
+                }
+                update_dir = true;
                 break;
             case KEY_UP:
                 if ( choice != 0 )
